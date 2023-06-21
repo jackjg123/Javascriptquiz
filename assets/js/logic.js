bf53eda70d5aa5ed0835fc97575f49d4963b3f8c
Jack <131559096+jackjg123@users.noreply.github.com>
2023-06-21 02:41:36 -0400

    Fixed the if statments of checkforEnter function. Edited starting time. added console.logs to startQuiz function. removed timer id = 10 statement. added code to update title. reworked clockTick() function. added if statement for checking for empty value. added parse statement. umcommented the code.

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -24,22 +24,23 @@ var feedbackEl = document.getElementById('feedback');
 function startQuiz() {
   //sets startScreenEl = to the value of startScreen ID.
   var startScreenEl = document.getElementById('start-screen');
+  console.log(startScreenEl);
   //sets startScreenEl = hide
   startScreenEl.setAttribute('class', 'hide');
-
+  console.log('Start Screen is now hidden');
   // sets questionEl = to the value of question ID.
   var questionsEl = document.getElementById('questions');
+  console.log(questionsEl);
   // removes hide form questionsEl
   questionsEl.removeAttribute('class', 'hide');
-
+  console.log('A question has appeared');
   //sets timerId to repeatedly execute clockTick function every 1 second
-  var timerId = setInterval(clockTick, 1000);
-
-  timerId = 10;
+  timerId = setInterval(clockTick, 1000);
+  console.log(timerId);
 
   // Show starting time
-  var timerEl = document.getElementById('time');
-  timerEl.textContent = time;
+  time = document.getElementById('time');
+  timerId.textContent = time;
 
   // Get question
   getQuestion();
@@ -48,11 +49,12 @@ function startQuiz() {
 function getQuestion() {
   // get current question object from array
   var presentQuestion = questionsEl[presentQuestionIndex];
-  console.log(presentQuestion);
 
   // update title with current question
   var titleEl = document.getElementById('questions');
+  presentQuestion = questionsEl;
   titleEl.textContent = presentQuestion.title; //think dot notation
+  console.log(titleEl.textContent);
 
   // clear out any old question choices
   choicesEl.innerHTML = '';
@@ -72,102 +74,110 @@ function getQuestion() {
   }
 }
 
-// function questionClick(event) {
-//   var buttonEl = event.target;
+function questionClick(event) {
+var buttonEl = event.target;
 
-//   // if the clicked element is not a choice button, do nothing.
-//   if (!buttonEl.matches('.choice')) {
-//     return;
-//   }
+// if the clicked element is not a choice button, do nothing.
+if (!buttonEl.matches('.choice')) {
+return;
+}
 
-//   // check if user guessed wrong
-//   if () {
+// check if user guessed wrong
+if () {
 
-//     // penalize time
+// penalize time minus one second
 
-//     // display new time on page
+// display new time on page
 
-//   // flash right/wrong feedback on page for half a second
+// flash right/wrong feedback on page for half a second
 
-//   // move to next question
+// move to next question
 
-//   // check if we've run out of questions or if time ran out?
-//   if () {
+// check if we've run out of questions or if time ran out?
+if () {
 
 //     //if it did ???
 
-//   } else {
+} else {
 
 //     // if it didnt??
-//   }
-// }
+}
+}
 
-// function quizEnd() {
-//   // stop timer
+function quizEnd() {
+// stop timer
 
-//   // show end screen
-//   var endScreenEl = document.getElementById('');
-//   endScreenEl.removeAttribute('class');
+// show end screen
+var endScreenEl = document.getElementById('');
+endScreenEl.removeAttribute('class');
 
-//   // show final score
-//   var finalScoreEl = document.getElementById('');
-//   finalScoreEl.textContent = time;
+// show final score
+var finalScoreEl = document.getElementById('');
+finalScoreEl.textContent = time;
 
-//   // hide questions section
-// }
+// hide questions section
 
-// function clockTick() {
-//   // update time
-//   // decrement the variable we are using to track time
-//   timerEl.textContent = ; // update out time
+}
 
-//   // check if user ran out of time
-//   if (time <= 0) {
-//     quizEnd();
-//   }
-// }
+function clockTick() {
+  // update time
+  time--;
+    // decrement the variable we are using to track time
+    timerId.textContent = time; // update out time
 
-// function saveHighScore() {
-//   // get value of input box
-//   var initials = initialsEl.value.trim();
+    // check if user ran out of time
+    if (time <= 0) {
+      console.log('Times up!');
+      quizEnd();
+    }
+  }
+}
+function saveHighScore() {
+// get value of input box
+var initials = initialsEl.value.trim();
 
-//   // make sure value wasn't empty
-//   if () {
+// make sure value wasn't empty
+if (initials > '') {
 
-//     // get saved scores from localstorage, or if not any, set to empty array
 
-//     var highScores =
-//       JSON.parse() /* what would go inside the PARSE??*/ || [];
+var highScores =
+JSON.parse(window.localStorage.getItem(highscores));
 
-//     // format new score object for current user
-//     var newScore = {
-//       score: time,
-//       initials: initials,
-//     };
+// format new score object for current user
+var newScore = {
+score: time,
+initials: initials,
+};
 
 //     // save to localstorage
-//     highScores.push(newScore);
-//     window.localStorage.setItem('highScores', JSON.stringify(/* What would we put inside STRINGIFY? */));
+highScores.push(newScore);
+ window.localStorage.setItem('highScores', JSON.stringify(/* What would we put inside STRINGIFY? */));
 
-//     // redirect to next page
-//     window.location.href = '';
-//   }
-// }
+// redirect to next page
+window.location.href = '';
+}
+}
 
-// function checkForEnter(event) {
+function checkForEnter(event) {
 //   // "13" represents the enter key
-//   if (event.key === 'Enter') {
-//     saveHighScore();
-//   }
-// }
+if (event.key === 'Enter') {
+saveHighScore();
+}
+}
 
-// // user clicks button to submit initials
-// submitBtn.onclick = saveHighScore;
+ // user clicks button to submit initials
+submitBtn.onclick = function() {
+saveHighScore();
+};
 
 // // user clicks button to start quiz
-startBtn.onclick = startQuiz;
+startBtn.onclick = function() {
+  startQuiz();
+}
 
 // user clicks on element containing choices
-// choicesEl.onclick = questionClick;
+choicesEl.onclick = function() {
+  getQuestion();
+};
 
-// initialsEl.onkeyup = checkForEnter;
+initialsEl.addEventListener('keyup', checkForEnter);
